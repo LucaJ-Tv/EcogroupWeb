0562ba06a6fca6443d14053900a1c7f6c1351f50
LucaFerar <luca.ferartofan@studio.unibo.it>
2024-03-28 16:34:50 +0100

    optimized docker image

--- ecogroup/src/views/prove.vue
+++ ecogroup/src/views/prove.vue
@@ -1,8 +1,9 @@
 <template>
-  <button @click="restoreSession" class="border-green-700 bg-green-700 shadow-md border rounded-xl p-2 mt-2 hover:bg-green-800 cursor-pointer w-full">restore</button>
+  <button @click="restoreSession" class="border-green-700 bg-blue-700 shadow-md border rounded-xl p-2 mt-2 hover:bg-green-800 cursor-pointer w-full">restore</button>
   <div>
     <p>SESSION: {{ session }}</p>
   </div>
+  <p>ciao bello!!</p>
 </template>
 
 <script>
--- mysql.dockerfile
+++
@@ -1,5 +0,0 @@
-FROM php:8.0-apache
-# Install your extensions to connect to MySQL and add mysqli
-RUN docker-php-ext-install mysqli
-# Install pdo is you need to use PHP PDO
-RUN docker-php-ext-install pdo pdo_mysql
