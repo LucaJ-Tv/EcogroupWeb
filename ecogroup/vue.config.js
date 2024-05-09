b0487c6d01ee39cb4c5d4fca485220587f6335eb
LucaFerar <luca.ferartofan@studio.unibo.it>
2024-05-09 16:31:43 +0200

    fix dipendenza da vue cli del progetto

--- ecogroup/vue.config.js
+++ ecogroup/vue.config.js
@@ -1,7 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require("path");
+
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "../dist/"),
+  assetsDir: "../dist/",
   publicPath: process.env.NODE_ENV === 'production'
     ? '/www/dist/'
     : '/'
